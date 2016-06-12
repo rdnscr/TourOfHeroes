eccfe8609044f95cf4cd3a7a9119d6eca5fbb7b0
rdnscr <raphael.ritter@bluewin.ch>
2016-06-12 17:18:22 +0200

    The Hero Editor: Show Hero object

--- src/app/tour-of-heroes.component.ts
+++ src/app/tour-of-heroes.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  constructor(public id: number, public name: string){
+    
+  }
+}
+
 @Component({
   moduleId: module.id,
   selector: 'tour-of-heroes-app',
@@ -7,5 +13,6 @@ import { Component } from '@angular/core';
   styleUrls: ['tour-of-heroes.component.css']
 })
 export class TourOfHeroesAppComponent {
-  title = 'tour-of-heroes works!';
+  title = 'Tour of Heroes with angular CLI!';
+  hero = new Hero(1, 'Windstorm');
 }
